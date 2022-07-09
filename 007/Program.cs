b30b372278143920c4a689f6d6328519dd660d2b
Alex <alex.kojokari42@gmail.com>
2022-07-09 19:55:29 +0300

    add Task007

---
+++ 007/Program.cs
@@ -0,0 +1,15 @@
+﻿//Выяснить является ли число чётным.
+
+double a;
+System.Console.WriteLine("Please insert a number");
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+
+if (a%2==0)
+{
+    System.Console.WriteLine($"{a} is an even number");
+}
+else
+{
+    System.Console.WriteLine($"{a} isn't an even number");
+}
