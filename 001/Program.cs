ac3cd91ca60e870d39e6e54fb250245697503df5
Alex <alex.kojokari42@gmail.com>
2022-07-09 15:50:16 +0300

    add Task001

---
+++ 001/Program.cs
@@ -0,0 +1,8 @@
+﻿//С клавиатуры вводится целое число. Вывести квадрат числа
+int a;
+int b;
+System.Console.WriteLine("Please insert a number");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
