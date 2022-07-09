63998a94ac2b10abc2d04b7540331daca2f0d121
Alex <alex.kojokari42@gmail.com>
2022-07-09 20:57:17 +0300

    add Task010

---
+++ 010/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+
+int a;
+System.Console.WriteLine("Please insert a number");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine();
+int c=a%10;
+System.Console.WriteLine(c);
