0fd0564bbb5b4cec23e62716297e8441547d0c0e
Alex <alex.kojokari42@gmail.com>
2022-07-09 17:42:47 +0300

    add Task003

---
+++ 003/Program.cs
@@ -0,0 +1,19 @@
+﻿//С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+double a;
+double b;
+System.Console.WriteLine("Please insert the first number");
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+System.Console.WriteLine("Please insert the second number");
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a>b)
+{
+    System.Console.WriteLine("First number is the biggest");
+}
+else
+{
+    System.Console.WriteLine("Second number is the biggest");
+}
