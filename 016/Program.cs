50dc9a8f4de7d96c92481000b247a9eee0ffc44b
Alex <alex.kojokari42@gmail.com>
2022-07-10 15:05:06 +0300

    add Task016

---
+++ 016/Program.cs
@@ -0,0 +1,14 @@
+﻿//Дано число. Проверить кратно ли оно 7 и 23.
+
+bool b, c, d;
+int x = 7 * 23;
+b = true;
+b = false;
+d = b && c;
+
+b = x % 7 == 0;
+c = x % 23 == 0;
+System.Console.WriteLine(x);
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+System.Console.WriteLine(d);
