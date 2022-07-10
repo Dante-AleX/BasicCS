0d4ec7edec19676864397e94650b023569204928
Alex <alex.kojokari42@gmail.com>
2022-07-10 15:44:49 +0300

    add Task019

---
+++ 019/Program.cs
@@ -0,0 +1,9 @@
+﻿//Проверить истинность утверждения ¬(x ⋁ y) = ¬x ⋀ ¬y. 
+//!(x || y)==!x && !y.
+
+bool x,y,f1,f2,f;
+x=true;
+y=true;
+f1=!(x || y);
+f2=!x && !y;
+System.Console.WriteLine($"{x} {y} {f1} {f2}");
