ff3c9d36b8507d8356cad90bf699194e4d691d19
Alex <alex.kojokari42@gmail.com>
2022-07-11 16:31:02 +0300

    add Task012

---
+++ 012/Program.cs
@@ -0,0 +1,19 @@
+﻿//Дано число из  диапозона  [10, 99]. Показать наибольшую цифру числа.
+
+int a = new Random().Next(10, 99);
+System.Console.WriteLine(a);
+int Digit1;
+int Digit2;
+
+Digit1 = a / 10 % 10;
+Digit2 = a % 10;
+System.Console.WriteLine($"The first digit is {Digit1}");
+System.Console.WriteLine($"The second digit is {Digit2}");
+    if (Digit1>Digit2)
+    {
+        System.Console.WriteLine($"{Digit1} is the biggest digit");
+    }
+        else
+            {
+                System.Console.WriteLine($"{Digit2} is the biggest digit");
+            }
--- 016/Program.cs
+++ 016/Program.cs
@@ -1,14 +1,12 @@
 ﻿//Дано число. Проверить кратно ли оно 7 и 23.
 
-bool b, c, d;
+bool b, c;
 int x = 7 * 23;
 b = true;
 b = false;
-d = b && c;
 
 b = x % 7 == 0;
 c = x % 23 == 0;
 System.Console.WriteLine(x);
 System.Console.WriteLine(b);
 System.Console.WriteLine(c);
-System.Console.WriteLine(d);
