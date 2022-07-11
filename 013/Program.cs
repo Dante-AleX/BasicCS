bd0ca58d318994af168b08768acec53846968b8f
Alex <alex.kojokari42@gmail.com>
2022-07-11 17:06:56 +0300

    add Task013

---
+++ 013/Program.cs
@@ -0,0 +1,11 @@
+﻿//Удалить вторую цифру целого числа введенного с клавиатуры.
+
+System.Console.WriteLine("Please insert a whole number");
+int a=Convert.ToInt32(Console.ReadLine());
+int Digit;
+
+Digit = a/10 % 10;
+
+System.Console.WriteLine(Digit);
+
+//Код работает с двухзначными числами. Как сделать лучше пока не знаю(
