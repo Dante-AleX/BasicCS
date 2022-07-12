31fbc64e7c1b9b2c7ddae6f6cec09f3a79c6edd8
Alex <alex.kojokari42@gmail.com>
2022-07-12 17:13:42 +0300

    add Task 015

--- 011/Program.cs
+++ 011/Program.cs
@@ -1,6 +1,5 @@
 ﻿//Дано двухзначное число. Вывести на экран  вторую цифру числа с конца.
 
-int a=4213;
-int b=a/10;
-int c=a/10%10;
-System.Console.WriteLine(c);
+int a=42134;
+int b=a/10%10;
+System.Console.WriteLine(b);
---
+++ 015/Program.cs
@@ -0,0 +1,21 @@
+﻿//С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+
+System.Console.WriteLine("Please insert a number");
+int a = Convert.ToInt32(Console.ReadLine());
+int d = ThirdDigit(a);
+Console.WriteLine($"{d} is the third digit of {a}");
+
+
+int ThirdDigit(int k)
+{
+    while (k > 1000)
+    k /= 10;
+    int d = k % 10;
+    return d;
+}
+if (a<100)
+System.Console.WriteLine("There is no third digit");
+
+/*Код работает но с одной оговоркой. Если вводить двухзначное число, то он пишет что нет 3 цифры, 
+но так же показывает что 2 цифра это якобы 3 цифра числа.
+*/
