6779a4619342eac05589f9095c66b5b89fd3a59b
Alex <alex.kojokari42@gmail.com>
2022-07-16 16:08:15 +0300

    add Task036, Task039, Task040

---
+++ 036/Program.cs
@@ -0,0 +1,18 @@
+﻿//Определить, присутствует ли в заданном массиве, некоторое число.
+
+int N=12;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(-9,10);
+
+int f=400;
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+//Linear Search
+int j=0;
+while (j<a.Length && a[j]!=f) j++;
+
+if (j==a.Length) System.Console.WriteLine("\nNot found");
+else System.Console.WriteLine($"\nElement {f} finded at {j}");
---
+++ 039/Program.cs
@@ -0,0 +1,18 @@
+﻿//Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+
+int N=12;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(-9,10);
+
+int f=400;
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int s=0;
+for(int i=0;i<a.Length;i++)
+    if (i%2!=0)
+        s=s+a[i];
+
+System.Console.WriteLine(s);
