f9182287ec60077ec1189004f1baeca150a42558
Alex <alex.kojokari42@gmail.com>
2022-07-21 13:45:24 +0300

    add Task 025

---
+++ 025/Program.cs
@@ -0,0 +1,14 @@
+﻿//Вывести на экран кубы чисел от 1 до N.
+
+Console.WriteLine("Please insert a whole number from 2 to N");
+int n = Convert.ToInt16(Console.ReadLine());
+
+if (n < 1)
+{
+    Console.WriteLine($"{n} isn't a whole number");
+    return;
+}
+for ( int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+}
--- 032/Program.cs
+++ 032/Program.cs
@@ -1,11 +1,23 @@
 ﻿//Задать массив из 8 элементов и вывести их на экран.
 
-int[] t;
-t = new int[8];
-t[0]=10;
-t[1]=12;
+int[] t;//объявление
+//инициализация
+Init(out t,8,max:100);
+//решение задачи
+//вывод результатов на экран
+Print(t,"t");
 
-for (int i=0; i<t.Length; i++)
-    t[i]=i*2;
-for (int i=0; i<t.Length; i++)
-System.Console.WriteLine($"a[{i}]={t[i]} ");
+
+void Init(out int[] t, int Length,int min=0,int max=10)
+{
+  t=new int[Length];
+  Random random=new Random();
+  for(int i=0;i<t.Length;i++) 
+    t[i]=random.Next(min,max+1);
+}
+
+void Print(int[] t, string variableName)
+{
+  for(int i=0;i<t.Length;i++)
+  System.Console.Write($"{variableName}[{i}]={t[i]} ");
+}
