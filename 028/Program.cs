ee6c764b6d0dab6fda40e1e2c91863c0d01a3176
Alex <alex.kojokari42@gmail.com>
2022-07-21 15:35:23 +0300

    add Task029

---
+++ 028/Program.cs
@@ -0,0 +1,11 @@
+﻿//Определить количество цифр в числе. Сделать подпрограмму.
+
+System.Console.WriteLine("Please insert a whole number");
+int N=Convert.ToInt32(System.Console.ReadLine());
+int k=0;
+while (N!=0)
+{
+    k++;
+    N=N/10;
+}
+System.Console.WriteLine(k);
---
+++ 029/Program.cs
@@ -0,0 +1,11 @@
+﻿//Подсчитать сумму цифр в числе.
+
+System.Console.WriteLine("Please insert a whole number");
+int a = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{
+    s = s + a % 10;
+    a = a / 10;
+}
+Console.WriteLine(s);
