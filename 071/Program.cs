dc44b43202c46a6dc795cf94d089c65b4b21f536
Alex <alex.kojokari42@gmail.com>
2022-08-21 11:19:38 +0300

    add Task071

---
+++ 071/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления функции Аккермана.
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt16(Console.ReadLine());
+
+if (number < 0 || number1 < 0) Console.WriteLine("Введите положительные числа.");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return (n + 1);
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m, n);
+}
+Console.WriteLine(Akkerman(number, number1));
