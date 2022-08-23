8bc9032a9205d608860674ce62ae6ecd8f2fbbbc
Alex <alex.kojokari42@gmail.com>
2022-08-23 12:29:33 +0300

    add Task066

---
+++ 066/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = 1;
+int N = 10;
+int Sum = 0;
+
+System.Console.WriteLine($"{M} {N}");
+
+while (M<=N)
+{
+    Sum = Sum + M;
+    M++;
+}
+System.Console.WriteLine(Sum);
